90b03a2620491b58ed2179bb54ac04a3931c4ef9
r.saji <r.saji@no4.co.jp>
2024-04-30 13:23:12 +0900

    WPF/MVVMサンプル（足し算）

---
+++ Views/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MVVMTest.ViewModels;
+using System.Windows;
+
+namespace MVVMTest.Views
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainWindowViewModel();
+        }
+    }
+}
